c1394979296037c10cf632957d54bcf397992c6a
Dell <abdoo.chbrik@gmail.com>
2021-05-04 19:14:13 +0200

    append generatekeypair() to generate a private key and pub key for our wallet

--- src/first/Wallet.java
+++ src/first/Wallet.java
@@ -1,5 +1,31 @@
 package first;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
+	
+	public PrivateKey pvkey;
+	public PublicKey pbkey;
+	public Wallet(){
+		
+		generateKeyPair();	
+	}
+		
+	public void generateKeyPair() {
+		try {
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+			// Initialize the key generator and generate a KeyPair
+			keyGen.initialize(ecSpec, random);   //256 bytes provides an acceptable security level
+	        	KeyPair keyPair = keyGen.generateKeyPair();
+	        	// Set the public and private keys from the keyPair
+	        	pvkey = keyPair.getPrivate();
+	        	pbkey = keyPair.getPublic();
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 
 }
